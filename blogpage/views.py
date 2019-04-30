9dba08eac64390ee953c04d10b929f889a654288
Mahesh <maheshchowdary091@gmail.com>
2019-04-30 11:07:09 +0530

    This is the first commit

---
+++ blogpage/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+# Create your views here.
+
+posts = [
+    {
+        'Writer' : 'Mahesh',
+        'title' : 'My story',
+        'content' : 'This is my first blog',
+        'date_posted' : 'Today'
+    },
+    {
+        'Writer': 'Ashraf',
+        'title': 'Ashraf\'s story',
+        'content': 'This is Ashraf\'s first blog',
+        'date_posted': 'Yesterday'
+    },
+{
+        'Writer': 'Masthan',
+        'title': 'Masthan\'s story',
+        'content': 'This is masthan\'s first blog',
+        'date_posted': 'Apr-30, 2019'
+    }
+]
+def home(request):
+    con = {
+        'posts':posts
+    }
+    return render(request, 'blogpage/home.html', con)
+
+def about(request):
+    return render(request, 'blogpage/about.html', {'title':'AboutPage'})
+
