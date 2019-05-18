67ba5519e3e9a4e262faab0fc66a63203d57549f
Mahesh <maheshchowdary091@gmail.com>
2019-05-19 01:00:28 +0530

    Created user app(latest)

--- blogpage/views.py
+++ blogpage/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from.models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'Writer' : 'Mahesh',
-        'title' : 'My story',
-        'content' : 'This is my first blog',
-        'date_posted' : 'Today'
-    },
-    {
-        'Writer': 'Ashraf',
-        'title': 'Ashraf\'s story',
-        'content': 'This is Ashraf\'s first blog',
-        'date_posted': 'Yesterday'
-    },
-{
-        'Writer': 'Masthan',
-        'title': 'Masthan\'s story',
-        'content': 'This is masthan\'s first blog',
-        'date_posted': 'Apr-30, 2019'
-    }
-]
+# posts = [
+#     {
+#         'Writer' : 'Mahesh',
+#         'title' : 'My story',
+#         'content' : 'This is my first blog',
+#         'date_posted' : 'Today'
+#     },
+#     {
+#         'Writer': 'Ashraf',
+#         'title': 'Ashraf\'s story',
+#         'content': 'This is Ashraf\'s first blog',
+#         'date_posted': 'Yesterday'
+#     },
+# {
+#         'Writer': 'Masthan',
+#         'title': 'Masthan\'s story',
+#         'content': 'This is masthan\'s first blog',
+#         'date_posted': 'Apr-30, 2019'
+#     }
+# ]
+
 def home(request):
     con = {
-        'posts':posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blogpage/home.html', con)
 
